e2344c7d0c6f2202b340bf82636dad6a1791ce31
Naratna <antar.espadas@hotmail.com>
2022-06-27 01:07:48 -0500

    Added fallback for missing host and port

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,6 +1,6 @@
 export async function setServerPause(paused: boolean) {
-    const host = import.meta.env.VITE_IPC_HOST
-    const port = import.meta.env.VITE_IPC_PORT
+    const host = import.meta.env.VITE_IPC_HOST ?? "127.0.0.1"
+    const port = import.meta.env.VITE_IPC_PORT ?? 8888
     const endpoint = paused ? "pause" : "play"
     const response = await fetch(`http://${host}:${port}/${endpoint}`)
     if (response.status === 200)
